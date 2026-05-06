d4415f2955be00fc9fb2b64adb60b68dfb706d5e
Forkastr <INFO@ENDIRECTUSA.COM>
2026-05-06 11:22:28 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" media="print" onLoad={(e: any) => { e.target.media='all'; }}/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
